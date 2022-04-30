39eace68d8c52d2bb7f027980720fc425c655370
kevinguyodo <kevin.guyodo@ynov.com>
2022-04-30 20:28:34 +0200

    Creation of game_info table

--- TicTacToe/BDD.py
+++ TicTacToe/BDD.py
@@ -1,4 +1,6 @@
 import sqlite3
+import datetime
+
 
 class BDD:
 
@@ -14,7 +16,16 @@ class BDD:
 
     def create_player_table(self):
         self.conn.execute("CREATE TABLE IF NOT EXISTS Player (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Username TEXT)");
-        print ("Table created successfully")
+        print ("Table Player created successfully")
+
+    def create_game_info(self):
+        self.conn.execute("CREATE TABLE IF NOT EXISTS Game_info ("
+                          "Player_id INTEGER, "
+                          "Opponent_id INTEGER NOT NULL, "
+                          "Result BOOLEAN NOT NULL,"
+                          "Start_time timestamp, "
+                          "FOREIGN KEY (Player_id) REFERENCES Player(ID))")
+        print("Table Game_info created successfully")
 
 
 
@@ -23,7 +34,13 @@ class BDD:
     def insert_player(self, username):
         self.conn.execute("INSERT INTO Player (Username) VALUES (?)", (username,))
         self.conn.commit()
-        print ("Records created successfully")
+        print("Records created successfully player")
+
+    def insert_game_info(self, player_id, opponent_id, result):
+        self.conn.execute("INSERT INTO Game_info (Player_id, Opponent_id, Result, Start_time) VALUES(?,?,?,?)", (player_id, opponent_id, result, datetime.datetime.now()))
+        self.conn.commit()
+        print("Records created successfully game info")
+
 
 
 
@@ -32,8 +49,17 @@ class BDD:
     def select_player(self):
         cursor = self.conn.execute("SELECT * FROM Player")
         for row in cursor:
-            print ("ID = ", row[0])
-            print ("Username = ", row[1])
+            print("ID = ", row[0])
+            print("Username = ", row[1])
+
+    def select_game_info(self):
+        cursor = self.conn.execute("SELECT * FROM Game_info")
+        for row in cursor:
+            print("Player ID = ", row[0])
+            print("Opponent ID = ", row[1])
+            print("Result = ", row[2])
+            print("Start time = ", row[3])
+
 
 
     #? Close connection
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from TicTacToe.Game import Game
 from TicTacToe.Grid import Grid
-from TicTacToe.BDD import *
+from TicTacToe.BDD import BDD
 
 def main():
     #print(getter.name)
     #getter.play()
     db.create_player_table()
-    """ db.insert_player("Elouan") """
+    db.create_game_info()
+    #db.insert_player("Elouan")
+    #db.insert_player("Kevin")
+    #db.insert_game_info(1, 2)
+    db.insert_game_info(2, 1, 0)
     db.select_player()
+    db.select_game_info()
     db.close_connexion()
 
 
