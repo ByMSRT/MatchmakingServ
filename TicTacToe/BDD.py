9e3256fa6fea4d4cdf7e1b783a54e672af9c3b7d
kevinguyodo <kevin.guyodo@ynov.com>
2022-05-02 15:05:00 +0200

    insert data in db and filter them

--- TicTacToe/BDD.py
+++ TicTacToe/BDD.py
@@ -1,5 +1,6 @@
 import sqlite3
 import datetime
+from TicTacToe.Game import Game
 
 
 class BDD:
@@ -19,7 +20,7 @@ class BDD:
         print ("Table created successfully")
     
     def create_stats_table(self):
-        self.conn.execute("CREATE TABLE IF NOT EXISTS Stats (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, Player_ID INTEGER, Win INTEGER, Lose INTEGER, Tie INTEGER, FOREIGN KEY (Player_ID) REFERENCES Player(ID) ON DELETE CASCADE)");
+        self.conn.execute("CREATE TABLE IF NOT EXISTS Stats (Player_ID INTEGER, Win INTEGER, Lose INTEGER, Tie INTEGER, FOREIGN KEY (Player_ID) REFERENCES Player(ID) ON DELETE CASCADE)");
         print ("Table created successfully")
         
 
@@ -50,7 +51,7 @@ class BDD:
     def insert_stats(self, player_id, win, lose, tie):
         self.conn.execute("INSERT INTO Stats (Player_ID, Win, Lose, Tie) VALUES (?,?,?,?)", (player_id, win, lose, tie))
         self.conn.commit()
-        print ("Records created successfully")
+        print("Records created successfully")
 
 
     #? Select Table
@@ -58,23 +59,24 @@ class BDD:
     def select_player(self):
         cursor = self.conn.execute("SELECT * FROM Player")
         for row in cursor:
-            print ("ID = ", row[0])
-            print ("Username = ", row[1])
+            print()
+            print(f"Player {row[0]} = {row[1]} \n")
     
     def select_stats(self):
-        cursor = self.conn.execute("SELECT Stats.ID, Player_ID, Win, Lose, Tie, Username FROM Stats INNER JOIN Player ON Player.ID = Stats.Player_ID")
+        cursor = self.conn.execute("SELECT Player_ID, Win, Lose, Tie, Username FROM Stats INNER JOIN Player ON Player.ID = Stats.Player_ID")
         for row in cursor:
-            print ("ID = ", row[0])
-            print ("Player_ID = ", row[1])
-            print ("Win = ", row[2])
-            print ("Lose = ", row[3])
-            print ("Tie = ", row[4])
-            print ("Username = ", row[5])
-            print ("\n")  
+            print()
+            print("Player_ID = ", row[0])
+            print("Win = ", row[1])
+            print("Lose = ", row[2])
+            print("Tie = ", row[3])
+            print("Username = ", row[4])
+            print("\n")
 
     def select_game_info(self):
         cursor = self.conn.execute("SELECT * FROM Game_info")
         for row in cursor:
+            print()
             print("Player ID = ", row[0])
             print("Opponent ID = ", row[1])
             print("Result = ", row[2])
@@ -91,3 +93,47 @@ class BDD:
     def close_connexion(self):
         self.conn.close()
         print ("Closed database successfully")
+
+
+    # -------------------------- SQLite request -------------------------
+
+    def get_stats_of_player(self, username):
+        user_id = self.get_player_info("ID", "Username", username)
+        for test in str(user_id):
+            print(test[0])
+
+            request = self.conn.execute(
+                f"SELECT Player_ID, Win, Lose, Tie FROM Stats INNER JOIN Player ON Player.ID = Stats.Player_ID WHERE Stats.Player_ID = {test[0]}")
+            for player_stats in request:
+                print(f"Joueur : '{username}' \n"
+                      f"ID : {player_stats[0]} \n"
+                      f"Partie Gagné : {player_stats[1]} \n"
+                      f"Partie Perdu : {player_stats[2]} \n"
+                      f"Partie Nul : {player_stats[3]} \n")
+
+    def get_game_info(self, username):
+        user_id = self.get_player_info("ID", "Username", username)
+        for test in str(user_id):
+            request = self.conn.execute(
+                f"SELECT Player_id, Opponent_id, Result, Start_time FROM Game_info INNER JOIN Player ON Player.ID = Player_id WHERE Player_id = {test[0]}")
+            for game_stats in request:
+                print(f"Joueur : '{username}' \n"
+                      f"ID : {game_stats[0]} \n"
+                      f"Adversaire ID : {game_stats[1]} \n"
+                      f"Nom Adversaire : {self.get_player_info('Username', 'ID', game_stats[1])} \n"
+                      f"Resultat : {game_stats[2]} \n"
+                      f"Debut de partie : {game_stats[3]} \n")
+
+    def get_player_info(self, information, parameter, parameter_value):
+        user_info = None
+        if isinstance(parameter_value, int):
+            user_info = self.conn.execute(f"SELECT {information} FROM Player WHERE {parameter} = {parameter_value}")
+        else:
+            user_info = self.conn.execute(f"SELECT {information} FROM Player WHERE {parameter} = '{parameter_value}'")
+
+        for info in user_info:
+            return info[0]
+        #Return string and not tuplr
+
+
+game = Game("Tic Tac Toe")
--- main.py
+++ main.py
@@ -4,6 +4,50 @@ from TicTacToe.BDD import BDD
 from MatchMaking.Connection import Connection
 
 def main():
+    result_input = menu()
+    if result_input == 1:
+        print()
+        print("Info Player")
+        db.select_player()
+        print()
+        print("Game info")
+        db.select_game_info()
+        print()
+        print("Stat info")
+        db.select_stats()
+    elif result_input == 2:
+        getter.play()
+    elif result_input == 3:
+        username = input("Quel est votre pseudo ? ")
+        db.insert_player(username)
+    elif result_input == 4:
+        player_id = input("Quelle est votre ID ? ")
+        win = input("Combien de partie avez-vous win ? ")
+        loose = input("Combien de partie avez-vous loose ? ")
+        null = input("Combien avez-vous fait de partie nul ? ")
+        db.insert_stats(player_id, win, loose, null)
+    elif result_input == 5:
+        player_id = input("Quelle est votre ID ? ")
+        opponent_id = input("Quelle est l'ID de votre adversaire ? ")
+        result = input("Avez-vous gagné ? ")
+        db.insert_game_info(player_id, opponent_id, result)
+    elif result_input == 6:
+        db.create_player_table()
+        db.create_stats_table()
+        db.create_game_info()
+    elif result_input == 7:
+        username = input("Quel est votre pseudo ? ")
+        #user_id = input("Quel est votre ID ? ")
+        #name = db.get_player_info("Username", "ID", int(user_id))
+        #print(name)
+        #id = db.get_player_info("ID", "Username", username)
+        #print(id)
+        db.get_stats_of_player(username)
+        print("\n \n")
+        db.get_game_info(username)
+
+    db.close_connexion()
+
     #print(getter.name)
     #getter.play()
     #connect.get_client_ip()
@@ -17,7 +61,17 @@ def main():
     #db.select_game_info()
     #db.close_connexion()
 
+def menu():
 
+    print("1 : Info BDD")
+    print("2 : Play")
+    print("3 : Insert Player")
+    print("4 : Insert Stat")
+    print("5 : Insert Game Info")
+    print("6 : Create all table")
+    print("7 : Connect")
+    result = input("Que veux-tu faire ?")
+    return int(result)
 getter = Game('Tic Tac Toe')
 grid = Grid()
 db = BDD()
